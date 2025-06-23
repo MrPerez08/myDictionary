8770417df46ee29f603f524d7ca73a4596acaf91
Aidan <aidan.perez1608@gmail.com>
2025-06-23 16:24:26 -0400

    I keep asking myself, do I want to be simple and use tkinter? should I use whats comfortable with Java Swing? should I learn C++ in favor of Qt? I hate everything Im going to make a website atp.

--- test/app.py
+++ test/app.py
@@ -3,26 +3,37 @@ import tkinter as tk
 root = tk.Tk()
 root.title("myDictionary")
 root.geometry("1366x768")
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
 
 frame = tk.Frame(root, width=root.winfo_screenwidth(),height=root.winfo_screenheight(),borderwidth=2,relief="sunken")
 frame.pack_propagate(False)
 frame.pack(anchor="nw",padx=10, pady=10)
+
 
 label = tk.Label(frame, text="This is inside the frame")
 label.pack()
 
 
 
-
-
-
-''' BUTTON UTILIZATION
-def button_click():
+def createVocab():
     print("Button clicked!")
 
-button = tk.Button(root, text="Click Me", command=button_click)
+button = tk.Button(frame, text=" + ", command=createVocab)
 button.pack()
-'''
+
+
+
+
+def addDefinition():
+    print("Button clicked!")
+
+button = tk.Button(frame, text=" + ", command=addDefinition)
+button.pack()
+
+
+
 
 root.mainloop()
 
