905fc77e0d87bcd27f4afa8dfa61e36adf44d5e0
Aidan <aidan.perez1608@gmail.com>
2025-06-23 20:18:29 -0400

    purely js definition text input areas

--- test/script.js
+++ test/script.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-      document.getElementById('definition').addEventListener('keyup', function(e) {
+    document.getElementById('definition').addEventListener('keyup', function(e) {
         if (e.key === 'Enter') {
-          console.log('Working!');
+            console.log('Working!');
         }
-      });
     });
+});
+
+const newDefinition = document.createElement('input');
+newDefinition.placeholder="Add a definition"
+newDefinition.id="definition"
+document.getElementById('definitions').appendChild(newDefinition);
+
+
+
