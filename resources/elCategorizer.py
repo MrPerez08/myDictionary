b322d352181f77dd140e7661953f4d27766154a4
Aidan <aidan.perez1608@gmail.com>
2026-02-02 08:24:10 -0500

    Word composition handling within the try statement

--- resources/elCategorizer.py
+++ resources/elCategorizer.py
@@ -10,12 +10,12 @@ class Word:
         self.vocab = word
         self.definition = definition
         
-    compositions = [[],[]]
+    compositions = []
     #First list contains the OBJECTS of the composed words and the second list contains the STRINGS of the composed words
+    #Is it necessary to contain that second list? no
     
     def add_Composition(self,vocab):
-        self.compositions[0].append(vocab)
-        self.compositions[1].append(vocab.word)
+        self.compositions.append(vocab)
 
 
 def redundancies(list):
@@ -27,36 +27,46 @@ def redundancies(list):
 
 
 
-
-
+#El testing twin
 x=Word("stupidity")
 y=Word("stupid")
+if y.vocab in x.vocab:
+    x.add_Composition(y)
 
-x.add_Composition(y)
-
-print(x.compositions[1])
-
-
+print(x.compositions[0].vocab)
 
 
 
-"""
 try:
     with open(file_path, 'r') as file:
         content = file.read()
         content = sorted(redundancies(content.splitlines()))
         new_list = [item for item in content if item != '']
+        
+        #List of Word instances generation
         obj_list=[]
         i=0
         while(len(new_list)>i):
             obj_list.append(Word(new_list[i]))
             i+=1
         
+        i=0
+        #Checking for word compositions
+        while(len(new_list)>i):
+            t=1
+            while(len(new_list)>t):
+                if(obj_list[t].vocab in obj_list[i].vocab):
+                    obj_list[i].add_Composition(obj_list[t])
+                t+=1
+            i+=1
+            
         for word in obj_list:
             print(word.vocab)
+            
+        
 except Exception as e:
     print(f"An error occurred: {e}")
-"""
+
     
     
     
